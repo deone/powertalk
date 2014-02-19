e96edd5d046eaec810a84a2c5bc6710afe88a894
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-02-19 13:30:27 +0100

    Use a more recent version of jquery. Move js to bottom of files.

--- js/quotefader.js
+++ js/quotefader.js
@@ -1,12 +1,10 @@
 function fadeQuotes()   {
-    $(".mast .quote_1").fadeIn().delay(4000).fadeOut();
-    $(".mast .quote_2").delay(5000).fadeIn(1000).delay(4000).fadeOut();
-    $(".mast .quote_3").delay(11000).fadeIn(1000).delay(4000).fadeOut();
-    setTimeout("fadeQuotes()", 17000);
+    $('.mast .quote_1').fadeIn().delay(4000).fadeOut();
+    $('.mast .quote_2').delay(5000).fadeIn(1000).delay(4000).fadeOut();
+    $('.mast .quote_3').delay(11000).fadeIn(1000).delay(4000).fadeOut();
+    setTimeout('fadeQuotes()', 17000);
 }
 
-$(function()    {
-
+$(document).ready(function()    {
     fadeQuotes();
-
 });
